16306b2de7700d91ad00105fc3e57e13c1bdceb0
Lorena N <lorena.1701814799@ucaldas.edu.co>
2023-07-13 17:37:04 -0500

    Modifications 1.1

--- src/envio.js
+++ src/envio.js
@@ -1,9 +1,16 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzX2Wdst9LvB7eI-N-lmEZf7uwhX5svY3NEoy_VKArTpZyprH99OCSbs2KA7tPq47Qp/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxMJhP9wbiRaNBoFV2-z4JabJOba1ImW14cUmMVIXxjqcqEKiQik4AOiMK6Q4VnYfrkDg/exec'
 const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
 
 form.addEventListener('submit', e => {
-e.preventDefault()
-fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => console.log('Success!', response))
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML= "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML= ""
+        },5000)
+        form.reset()
+    })            
     .catch(error => console.error('Error!', error.message))
 })
